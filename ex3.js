c23c790f6d01429a02bbdec049da6a8ca6f61476
changwanlin <411630816@o365.tku.edu.tw>
2024-10-29 22:29:20 +0800

    add ex3.ts

--- ex3.js
+++ ex3.js
@@ -6,14 +6,10 @@
 每一行應輸出類似格式：i x j = (i * j)，例如：2 x 3 = 6。
 呼叫該函數來列印完整的乘法表。*/
 function printMultiplicationTable() {
-    // 外層迴圈控制乘數
     for (var i = 1; i <= 9; i++) {
-        // 內層迴圈控制被乘數
         for (var j = 1; j <= 9; j++) {
-            // 印出當前的乘法表運算結果
             console.log("".concat(i, " x ").concat(j, " = ").concat(i * j));
         }
     }
 }
-// 呼叫函數來列印九九乘法表
 printMultiplicationTable();
